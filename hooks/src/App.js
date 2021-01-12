1fb9ac97aeccf0e7f148073f304be26c4d8eb33b
daniel <jaelee9212@gmail.com>
2021-01-12 17:33:22 +0900

    feat: useState

---
+++ hooks/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+function App({initialCount}) {
+  // let [count, setCount] = useState(initialCount);
+  let [state, setState] = useState({
+    count: initialCount,
+    name: 'Francis'
+  });
+
+  let [posts, setPosts] = useState([
+    {
+      name: 'awesome pos',
+      body: 'The content of the post'
+    },
+    {      
+      name: 'bad post',
+      body: 'No content'
+    }
+  ])
+
+  const addOne = () => {
+    setState({...state, count: state.count + 1});
+  }
+
+  const restOne = () => {
+    setState({...state, count: state.count - 1});
+  }
+
+  const reset = () => {
+    setState({...state, count: initialCount});
+  }
+  
+
+  const addOnePost = () => {
+    let newPost = {
+      name: 'very good post',
+      body: 'No content at this time'
+    }
+
+    setPosts([...posts, newPost])
+  }
+  return (
+
+    <>
+      <div className="App">
+        <h2>{state.name}</h2>
+        <h3> Count : {state.count}</h3>
+        <button onClick={() => addOne() }>Add one</button>
+        <button onClick={ restOne }>Rest one</button>
+        <button onClick={ reset }>Reset</button>
+      </div>
+
+      <hr/>
+      <button onClick={ addOnePost }>Add one post</button>
+      { posts.map((item, i) => (
+        <>
+          <div key={i}>
+            <div>Name: {item.name}</div>
+            <div>Body: {item.body}</div>
+          </div>
+          <hr/>
+        </>
+      ))}
+    </>
+  );
+}
+
+export default App;
